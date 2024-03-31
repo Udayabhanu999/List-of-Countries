0c3cb2eb5ff024262af91d2844c3f410a8a4f1e8
Udayabhanu999 <maddineniudayabhanu2022@gmail.com>
2024-03-31 02:38:01 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RestExample from "./RestExample";
+
+ReactDOM.render(<React.StrictMode>
+  <RestExample/>
+</React.StrictMode>, document.getElementById('root'));
